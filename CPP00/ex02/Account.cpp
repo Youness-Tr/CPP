79c576c6a634cb3c1f433abbc4954fb80978c26a
ytarhoua <ytarhoua@c3r2p10.1337.ma>
2024-12-07 15:03:21 +0100

    CPP00 in progress

--- CPP00/ex02/Account.cpp
+++ CPP00/ex02/Account.cpp
@@ -46,3 +46,37 @@ void Account::displayAccountsInfos(void) {
     std::cout << "[ time ]::";
     std::cout << "accounts:" << _nbAccounts << ";total:" << _totalAmount << ";deposits:" << _totalNbDeposits << ";withdrawals:" << _totalNbWithdrawals << std::endl;
 }
+
+void	Account::makeDeposit( int deposit )
+{
+    std::cout << "[ time ]::";
+    std::cout << "index:" << _accountIndex << ";p_amount:" << _amount << ";deposit:" << deposit;
+    _amount += deposit;
+    _nbDeposits++;
+    _totalAmount += deposit;
+    _totalNbDeposits++;
+    std::cout << ";amount:" << _amount << ";nb_deposits:" << _nbDeposits << std::endl;
+}
+
+void Account::displayStatus(void) const
+{
+    std::cout << "[ time ]::";
+    std::cout << "index:" << _accountIndex << ";amount:" << _amount << ";deposits:" << _nbDeposits << ";withdrawals:" << _nbWithdrawals << std::endl;
+}
+
+bool Account::makeWithdrawal(int withdrawal)
+{
+    std::cout << "[ time ]::";
+    std::cout << "index:" << _accountIndex << ";p_amount:" << _amount << ";withdrawal:";
+    if (withdrawal > _amount)
+    {
+        std::cout << "refused" << '\n';
+        return(false);
+    }
+    _amount -+ withdrawal;
+    _totalNbWithdrawals++;
+    _totalAmount -= withdrawal;
+    _nbWithdrawals++;
+    std::cout << withdrawal << ";amount:" << _amount << ";nb_withdrawals:" << _nbWithdrawals << std::endl;
+    return(true);
+}
